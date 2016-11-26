42fed009639235bbcb6938191d8450c937b4b5d5
Cami Rodriguez <camiepisode@outlook.com>
2016-11-26 06:02:35 -0500

    Added basic project

---
+++ XamarinDevDays/XamarinDevDays/XamarinDevDays/Customer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinDevDays
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        private string _firstname;
+        private string _lastname;
+        private string _address;
+        private string _city;
+        private string _state;
+        private string _zip;
+
+        private string _result;
+        
+        public string FirstName
+        {
+            get { return _firstname; }
+            set
+            {
+                if(value != _firstname)
+                {
+                    _firstname = value;
+                    OnPropertyChanged("FirstName");
+                    OnPropertyChanged("HasValidInput");
+                }
+            }
+        }
+        
+
+        public string LastName
+        {
+            get { return _lastname; }
+            set
+            {
+                if (value != _lastname)
+                {
+                    _lastname = value;
+                    OnPropertyChanged("LastName");
+                    OnPropertyChanged("HasValidInput");
+                }
+            }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                if (value != _address)
+                {
+                    _address = value;
+                    OnPropertyChanged("Address");
+                }
+            }
+        }
+
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                if (value != _city)
+                {
+                    _city = value;
+                    OnPropertyChanged("City");
+                }
+            }
+        }
+
+        public string State
+        {
+            get { return _state; }
+            set
+            {
+                if (value != _state)
+                {
+                    _state = value;
+                    OnPropertyChanged("State");
+                }
+            }
+        }
+
+        public string Zip
+        {
+            get { return _zip; }
+            set
+            {
+                if (value != _zip)
+                {
+                    _zip = value;
+                    OnPropertyChanged("Zip");
+                }
+            }
+        }
+
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                if (value != _result)
+                {
+                    _result = value;
+                    OnPropertyChanged("Result");
+                }
+            }
+        }
+
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ XamarinDevDays/XamarinDevDays/XamarinDevDays/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinDevDays.ViewModels
+{
+    public class MainPageViewModel : Customer
+    {
+        public ICommand ShowNameCommand { get; private set; }
+        public ICommand ResetFieldsCommand { get; private set; }
+
+        public bool HasValidInput { get { return !String.IsNullOrWhiteSpace(FirstName) && !String.IsNullOrWhiteSpace(LastName); } }
+
+        public MainPageViewModel()
+        {
+            ShowNameCommand = new Command(() =>
+            {
+                Result = "Submited Data Succesfuly";
+            });
+
+            ResetFieldsCommand = new Command(() =>
+            {
+                FirstName = null;
+                LastName = null;
+                Address = null;
+                City = null;
+                State = null;
+                Zip = null;
+            });
+        }
+    }
+}
